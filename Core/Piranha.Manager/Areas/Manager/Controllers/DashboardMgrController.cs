830c6767d0d43d5e4c48b49fe43139fa5ab9c991
Håkan Edling <hakan@tidyui.com>
2014-12-05 21:11:10 +0100

    Added default manager route. Closes issue #28

---
+++ Core/Piranha.Manager/Areas/Manager/Controllers/DashboardMgrController.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Piranha CMS
+ * Copyright (c) 2014, Håkan Edling, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+using System;
+using System.Web.Mvc;
+
+namespace Piranha.Areas.Manager.Controllers
+{
+	/// <summary>
+	/// The main entry point for the manager.
+	/// </summary>
+	[Authorize]
+	[RouteArea("Manager", AreaPrefix = "manager")]
+    public class DashboardMgrController : ManagerController
+    {
+		/// <summary>
+		/// Default action for the dashboard.
+		/// </summary>
+		/// <returns>The result</returns>
+		[HttpGet]
+		[Route("")]
+		public ActionResult Get() {
+			return RedirectToAction("List", "PostMgr");
+		}
+    }
+}
